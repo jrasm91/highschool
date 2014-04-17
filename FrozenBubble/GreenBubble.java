3dd01aabb5ec630f49b491a8a0726e052f98853d
Jason Rasmussen <JRASM91@gmail.com>
2014-04-16 18:50:19 -0600

    added files

---
+++ FrozenBubble/GreenBubble.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GreenBubble here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GreenBubble extends Bubble
+{
+    /**
+     * Act - do whatever the GreenBubble wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+      
+}
---
+++ FrozenBubble/RedBubble.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RedBubble here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RedBubble extends Bubble
+{
+    /**
+     * Act - do whatever the RedBubble wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+ 
+}
